298404dbe3d3cf79647bc324bc245db36a790dbc
haddadianhamed1 <hamedh@dnanexus.com>
2018-02-10 14:29:03 -0800

    added compose

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from redis import Redis
+import os, socket
 
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+@app.route('/')
 def hello():
     return ("Hello world this is a test app")
+
+@app.route('/hello')
+def hello_world():
+   return 'hello world'
+
+@app.route('/inc')
+def hello_world1():
+   redis.incr('totalhits')
+   return 'hello from container %s ! I have been viewd %s times' %(socket.gethostname(),redis.get('totalhits'))
+
+
 
 
 if __name__ == "__main__":
